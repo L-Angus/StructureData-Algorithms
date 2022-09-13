818681defa14cc26e0661cd309b268281b120aec
angus.li <lsd15651008321@163.com>
2022-09-13 17:32:32 +0800

    "Stack"

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,5 +1,41 @@
 //
 // Created by 李顺东 on 2022/9/13.
 //
-
+#include <iostream>
 #include "Stack.h"
+
+void Stack::InitStack(sqStack *S) {
+    S = new sqStack();
+    S->top = -1;
+    StackSize = S->top + 1;
+}
+
+int Stack::Push(sqStack *S, int e) {
+    if(S->top == MAXSIZE -1){
+        return FULLSTACK;
+    }
+    S->top++;
+    S->data[S->top] = e;
+    StackSize++;
+    return OK;
+}
+
+int Stack::Pop(sqStack *S) {
+    if(S->top == -1)
+        return EMPTYSTACK;
+    auto e = S->data[S->top];
+    S->top--;
+    StackSize--;
+    return e;
+}
+
+int Stack::Show(sqStack *S) {
+    if(S->top == -1)
+        return EMPTYSTACK;
+    while (StackSize > 0){
+        std::cout << S->data[S->top--] << " ";
+        StackSize--;
+    }
+    std::cout << std::endl;
+    return OK;
+}
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -4,13 +4,30 @@
 
 #ifndef STACK_STACK_H
 #define STACK_STACK_H
+#define MAXSIZE 20
 
+#define FULLSTACK -1
+#define OK 0
+#define EMPTYSTACK -100
 
+struct sqStack{
+    int data[MAXSIZE];
+    int top;
+};
 
 class Stack {
+public:
+    Stack(){}
+    ~Stack(){}
 
+    void InitStack(sqStack* S);
+    int Push(sqStack *S, int e);
+    int size(sqStack *S) { return StackSize; }
+    int Pop(sqStack* S);
+    int Show(sqStack *S);
+private:
+    int StackSize;
 };
 
 
-
-#endif //STACK_STACK_H
+#endif//STACK_STACK_H
